b3ae4d06bad093f46c4b7da6a3f98a13485d45b6
javidgregorio <javidgregorio@gmail.com>
2021-01-21 10:18:38 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -10,10 +10,10 @@ export class ContactPage {
   feet: number;
   fttoinch: number;
   inches: number;
-  rdesired: number;
-  mdesired: number;
-  hdesired: number;
-  ddesired: number;
+  robinsondesired: number;
+  millerdesired: number;
+  hamwidesired: number;
+  devinedesired: number;
   sex: string;
   pmessage: string;
     
@@ -21,22 +21,20 @@ export class ContactPage {
       
     calculatedesired() {
 
-    
-    
          if (this.sex == "m"){
       this.fttoinch = (this.feet - 5) * 12;
-      this.rdesired = Math.round(52 + 1.9 *(this.fttoinch)+ 1.9 * this.inches);
-      this.mdesired = Math.round(56.2 + 1.41 *(this.fttoinch)+ 1.41 * this.inches);
-      this.hdesired = Math.round(48 + 2.7 *(this.fttoinch)+ 2.7 * this.inches);
-      this.ddesired = Math.round(50 + 2.3 *(this.fttoinch)+ 2.3 * this.inches);
+      this.robinsondesired = Math.round(52 + 1.9 *(this.fttoinch)+ 1.9 * this.inches);
+      this.millerdesired = Math.round(56.2 + 1.41 *(this.fttoinch)+ 1.41 * this.inches);
+      this.hamwidesired = Math.round(48 + 2.7 *(this.fttoinch)+ 2.7 * this.inches);
+      this.devinedesired = Math.round(50 + 2.3 *(this.fttoinch)+ 2.3 * this.inches);
       this.pmessage = "kg.";
     }
     else if (this.sex == "f") {
       this.fttoinch = (this.feet - 5) * 12;
-      this.rdesired = Math.round(49 + 1.7 *(this.fttoinch)+ 1.7 * this.inches);
-      this.mdesired = Math.round(53.1 + 1.36 *(this.fttoinch)+ 1.36 * this.inches);
-      this.hdesired = Math.round(45.5 + 2.2 *(this.fttoinch)+ 2.2 * this.inches);
-      this.ddesired = Math.round(45.5 + 2.3 *(this.fttoinch)+ 2.3 * this.inches);
+      this.robinsondesired = Math.round(49 + 1.7 *(this.fttoinch)+ 1.7 * this.inches);
+      this.millerdesired = Math.round(53.1 + 1.36 *(this.fttoinch)+ 1.36 * this.inches);
+      this.hamwidesired = Math.round(45.5 + 2.2 *(this.fttoinch)+ 2.2 * this.inches);
+      this.devinedesired = Math.round(45.5 + 2.3 *(this.fttoinch)+ 2.3 * this.inches);
       this.pmessage = "kg.";
     }
     else{
